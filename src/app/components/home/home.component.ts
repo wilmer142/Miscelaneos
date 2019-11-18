1b215ad59f057c6e2dc0574486d5b70e17c822b5
wilmer142 <wilmercollege@gmail.com>
2019-11-18 10:20:11 -0500

    Example of lyfe cicle of component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,14 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges, DoCheck, AfterContentInit,
+          AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html'
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnChanges, DoCheck, AfterContentInit,
+AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
 
   constructor() { }
 
   ngOnInit() {
+    console.log("ngOnInit");
   }
+
+  ngOnChanges() {
+    console.log("ngOnChanges");
+  }
+
+  ngDoCheck() {
+    console.log("ngDoCheck");
+  }
+
+  ngAfterContentInit() {
+    console.log("ngAfterCont");
+  }
+
+  ngAfterContentChecked () {
+    console.log("ngAfterCont");
+  }
+
+  ngAfterViewInit() {
+    console.log("ngAfterView");
+  }
+
+  ngAfterViewChecked() {
+    console.log("ngAfterView");
+  }
+
+  ngOnDestroy() {
+    console.log("ngOnDestroy");
+  }
+
 
 }
